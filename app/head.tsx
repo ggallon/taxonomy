d2e8ea8206a4bbe387493b930a03d00231cd241c
Gwenaël Gallon <gwenael.gallon@mac.com>
2022-12-02 10:10:46 +0100

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Taxonomy</title>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width" />
+      <meta name="viewport" content="width=device-width, initial-scale=1"  />
       <meta
         name="description"
         content="An open source application built using the new router, server components and everything new in Next.js 13."
