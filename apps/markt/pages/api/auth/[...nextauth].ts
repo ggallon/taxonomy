a274ebf87f97a5abb99178ed8d00d07366207d28
Gwenaël Gallon <gwenael.gallon@mac.com>
2022-12-05 12:37:44 +0100

    feat: Allow Email Signups Behind Link Checker

--- apps/markt/pages/api/auth/[...nextauth].ts
+++ apps/markt/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from "next"
 import NextAuth from "next-auth"
 import { authOptions } from "@/lib/auth"
 
-// @see ./lib/auth
-export default NextAuth(authOptions)
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  // This should allow you to successfully use NextAuth.js's Email provider behind strict corporate IT settings.
+  // https://next-auth.js.org/tutorials/avoid-corporate-link-checking-email-provider
+  if (req.method === "HEAD") {
+    return res.status(200)
+  }
+
+  return await NextAuth(req, res, authOptions)
+}
