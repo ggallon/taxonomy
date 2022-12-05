9e8cb3534edc1a1493160d8ddf3b89ce9e190e07
Gwenaël Gallon <gwenael.gallon@mac.com>
2022-12-05 20:38:13 +0100

    refactor: use siteConfig in Head

--- apps/markt/app/head.tsx
+++ apps/markt/app/head.tsx
@@ -1,19 +1,18 @@
+import { siteConfig } from "@/config/site"
+
 export default function Head() {
   return (
     <>
-      <title>Markt</title>
+      <title>{siteConfig.name}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta
-        name="description"
-        content="An open source application built using the new router, server components and everything new in Next.js 13."
-      />
-      <meta property="og:title" content="Markt" />
-      <meta property="og:url" content="https://markt.vercel.app" />
-      <meta property="og:image" content="https://markt.vercel.app/og.jpg" />
+      <meta name="description" content={siteConfig.description} />
+      <meta property="og:title" content={siteConfig.name} />
+      <meta property="og:url" content={siteConfig.baseUrl} />
+      <meta property="og:image" content={siteConfig.defaultOgImg} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="Markt" />
-      <meta name="twitter:image" content="https://markt.vercel.app/og.jpg" />
+      <meta name="twitter:title" content={siteConfig.name} />
+      <meta name="twitter:image" content={siteConfig.defaultOgImg} />
     </>
   )
 }
--- apps/markt/config/site.ts
+++ apps/markt/config/site.ts
@@ -2,6 +2,10 @@ import { SiteConfig } from "types"
 
 export const siteConfig: SiteConfig = {
   name: "Markt",
+  description:
+    "An open source application built using the new router, server components and everything new in Next.js 13.",
+  baseUrl: "https://markt.vercel.app",
+  defaultOgImg: "https://markt.vercel.app/og.jpg",
   links: {
     twitter: "https://twitter.com/gwengallon",
     github: "https://github.com/ggallon/markt",
